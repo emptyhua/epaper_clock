bb24ad59b19cdd2c11281ad72c4632ae60d0ec39
cclauss <cclauss@bluewin.ch>
2016-04-26 14:31:50 +0200

    Simplify with str.format(**dict)

--- home_air_sensor.py
+++ home_air_sensor.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
-import sys, os
-import Adafruit_DHT as dht
+# coding: utf-8
+
 import json
+import os
 import time
-h,t = dht.read_retry(dht.DHT22, 4)
-result = {}
-result['temp']      = t
-result['humidity']  = h
-result['update']    = int(time.time())
-data_file = os.path.dirname(os.path.abspath(__file__)) + '/home_air.json'
-json.dump(result, file(data_file, 'w'))
+
+import Adafruit_DHT
+
+h, t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+result = {'temp': t, 'humidity': h, 'update': int(time.time())}
+data_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'home_air.json')
+with open(data_file, 'w') as out_file:
+    json.dump(result, out_file)
