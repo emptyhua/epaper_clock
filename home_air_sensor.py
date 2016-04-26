1646078d995419cfa470a11a0db1e7666163b581
emptyhua <emptyhua@gmail.com>
2016-04-26 22:01:15 +0800

    convert float value to int

--- home_air_sensor.py
+++ home_air_sensor.py
@@ -8,7 +8,7 @@ import time
 import Adafruit_DHT
 
 h, t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
-result = {'temp': t, 'humidity': h, 'update': int(time.time())}
+result = {'temp': int(t), 'humidity': int(h), 'update': int(time.time())}
 data_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'home_air.json')
 with open(data_file, 'w') as out_file:
     json.dump(result, out_file)
